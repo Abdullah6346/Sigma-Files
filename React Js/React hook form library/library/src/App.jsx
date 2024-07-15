c4f0331719dffc3f948e48ad3a44cbe9c52af3ee
Muhammad Abdullah <muhmmadabdullah6346@gmail.com>
2024-07-15 15:59:45 +0500

    validations completed

--- React Js/React hook form library/library/src/App.jsx
+++ React Js/React hook form library/library/src/App.jsx
@@ -14,12 +14,13 @@ function App() {
 
         <label htmlFor="username">
           User-Name:
-        <input  placeholder="Username"type="text"  {...register("username")} />
-
+        <input  placeholder="Username"type="text"  {...register("username",{required:{value:true,message:"this field is required"} , minLength:{value:2,message:"The minLength is 2"},maxLength:{value:6,message:"The maxLength is 6"}})} />
+        {errors.username && <div>{errors.username.message}</div>}
         </label>
         <label htmlFor="password">
           Password:
-        <input placeholder='Password' type="password"  {...register("password")}  />
+        <input placeholder='Password' type="password"  {...register("password",{required:{value:true,message:"this field is required"}, minLength:{value:2,message:"The minLength is 2"},maxLength:{value:6,message:"The maxLength is 6" } } )}/>
+        {errors.password && <div>{errors.password.message}</div>}
 
         </label>
         </div>
