32b2e14872bcb5e2bffd6d83238deaf4f272be69
muhammad-abdullah <muhmmadabdullah6346@gmail.com>
2024-07-23 16:00:52 +0500

    new

--- React Js/husky and commitlint/vite-project/src/App.jsx
+++ React Js/husky and commitlint/vite-project/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p className='Red'>Hi</p>
+      <p>blue</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
