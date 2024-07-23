5ea983a0e6008a89ba18bb52f1ecd136b607c0ed
Muhammad Abdullah <muhmmadabdullah6346@gmail.com>
2024-07-23 11:13:05 +0500

    New Project is created
    to learn eslint and pritter i created this project

--- React Js/Todo app with redux/todo-app/src/Redux/store.jsx
+++ React Js/Todo app with redux/todo-app/src/Redux/store.jsx
@@ -1,6 +1,35 @@
 import {configureStore} from '@reduxjs/toolkit';
 import todoReducer from '../features/todo/todoSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from "redux-persist";
 
-export const store = configureStore({
-    reducer: todoReducer
-})
+const persistConfig = {
+    key: "root",
+    storage,
+  };
+  
+
+  const persistedReducer = persistReducer(persistConfig, todoReducer);
+  export default () => {
+    const store = configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+      ],
+    });
+    const persistor = persistStore(store);
+    return { store, persistor };
+  };
--- React Js/Todo app with redux/todo-app/src/main.jsx
+++ React Js/Todo app with redux/todo-app/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './Redux/store'
+import configureStore from "./Redux/store";
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+const { store, persistor } = configureStore();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
+        <PersistGate persistor={persistor}>
+
     <App />
+        </PersistGate>
     </Provider>
 )
